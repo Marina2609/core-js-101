c35de3ba57f2f6fe9876373b0dcac0ed9c3f13e6
Marina2609 <78434449+Marina2609@users.noreply.github.com>
2022-01-14 10:36:21 +0300

    feat: solved several tasks in 05-objects-tasks

--- src/05-objects-tasks.js
+++ src/05-objects-tasks.js
@@ -7,7 +7,7 @@
  ************************************************************************************************ */
 
 
-/**
+/** +
  * Returns the rectangle object with width and height parameters and getArea() method
  *
  * @param {number} width
@@ -20,12 +20,16 @@
  *    console.log(r.height);      // => 20
  *    console.log(r.getArea());   // => 200
  */
-function Rectangle(/* width, height */) {
-  throw new Error('Not implemented');
+function Rectangle(width, height) {
+  this.width = width;
+  this.height = height;
+  this.getArea = function getArea() {
+    return width * height;
+  };
 }
 
 
-/**
+/** +
  * Returns the JSON representation of specified object
  *
  * @param {object} obj
@@ -35,12 +39,12 @@ function Rectangle(/* width, height */) {
  *    [1,2,3]   =>  '[1,2,3]'
  *    { width: 10, height : 20 } => '{"height":10,"width":20}'
  */
-function getJSON(/* obj */) {
-  throw new Error('Not implemented');
+function getJSON(obj) {
+  return JSON.stringify(obj);
 }
 
 
-/**
+/** +
  * Returns the object of specified type from JSON representation
  *
  * @param {Object} proto
@@ -51,8 +55,10 @@ function getJSON(/* obj */) {
  *    const r = fromJSON(Circle.prototype, '{"radius":10}');
  *
  */
-function fromJSON(/* proto, json */) {
-  throw new Error('Not implemented');
+function fromJSON(proto, json) {
+  const rezult = JSON.parse(json);
+  Object.setPrototypeOf(rezult, proto);
+  return rezult;
 }
 
 
